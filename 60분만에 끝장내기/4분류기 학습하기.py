a9f86b256eb5f15e9fda42c0dcf17bba7f2c961e
LulinPollux <kistssy+dev@gmail.com>
2020-04-01 17:42:19 +0900

    complete section 4

---
+++ 60분만에 끝장내기/4분류기 학습하기.py
@@ -0,0 +1,161 @@
+import torch
+import torchvision
+import torchvision.transforms as transform
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+'''
+    1. CIFAR10를 불러오고 정규화하기
+'''
+transform = transform.Compose(
+    [transform.ToTensor(),
+     transform.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# 재미삼아 학습용 이미지 몇 개를 보겠습니다.
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+'''
+    2. CNN 정의하기
+'''
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+net.to(device)
+
+
+'''
+3. 손실 함수와 Optimizer 정의하기
+'''
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+'''
+4. 신경망 학습하기
+'''
+for epoch in range(2):      # 데이터셋을 수차례 반복합니다.
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # [inputs, labels]의 목록인 data로부터 입력을 받은 후
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        # 변화도(Gradient) 매개변수를 0으로 만들고
+        optimizer.zero_grad()
+
+        # 순전파 + 역전파 + 최적화를 한 후
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # 통계를 출력합니다.
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # 2000개의 미니배치마다 출력
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# 학습한 모델을 저장한다.
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+
+'''
+5. 시험용 데이터로 신경망 검사하기
+'''
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# 이미지를 출력한다.
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# 저장했던 모델을 불러오기
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+# 이 예제들을 신경망이 어떻게 예측했는지 보기
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+# 전체 데이터셋에 대해서는 어떻게 동작하는지 보기
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+# 어떤 것들을 더 잘 분류하고, 어떤 것들을 더 못했는지 알아보기
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
