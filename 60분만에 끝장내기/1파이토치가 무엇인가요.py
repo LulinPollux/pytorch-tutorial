8049d4e0f662615e42053e904934abda913b2892
LulinPollux <kistssy+dev@gmail.com>
2020-04-01 13:58:15 +0900

    complete section 1

--- 60분만에 끝장내기/1파이토치가 무엇인가요.py
+++ 60분만에 끝장내기/1파이토치가 무엇인가요.py
@@ -3,6 +3,9 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
+'''
+    Tensors
+'''
 # 초기화되지 않은 5x3 행렬 생성
 x = torch.empty(5, 3).to(device)
 print(x)
@@ -28,6 +31,9 @@ print(x)
 # 행렬의 크기를 구함
 print(x.size())
 
+'''
+    연산 (Operations)
+'''
 # 덧셈 연산
 y = torch.rand(5, 3)
 print(x + y)
@@ -45,3 +51,42 @@ x = torch.randn(4, 4)
 y = x.view(16)
 z = x.view(-1, 8)   # -1은 다른 차원들을 사용하여 유추합니다.
 print(x.size(), y.size(), z.size())
+
+# .item()을 사용하면 숫자 값을 얻을 수 있음.
+x = torch.randn(1)
+print(x)
+print(x.item())
+
+'''
+    Numpy 변환
+'''
+# Torch Tensor를 Numpy 배열로 변환하기
+a = torch.ones(5)
+print(a)
+
+b = a.numpy()
+print(b)
+
+# CPU Torch Tensor와 NumPy 배열은 저장 공간을 공유하기 때문에, 하나를 변경하면 다른 하나도 변경됨.
+a.add_(1)
+print(a)
+print(b)
+
+# NumPy 배열을 Torch Tensor로 변환하기
+import numpy as np
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out=a)
+print(a)
+print(b)
+
+'''
+    CUDA Tensors
+'''
+# .to()를 사용하여 Tensor를 어떤 장치로도 이동시킬 수 있습니다. (move)
+y = torch.ones_like(x, device=device)   # GPU 상에 직접적으로 tensor를 생성
+x = x.to(device)    # .to()로 CPU에서 생성한 tensor를 GPU로 옮김
+z = x + y
+print(x, y)
+print(z)
+print(z.to("cpu", torch.double))    # .to()는 dtype도 함께 변경 가능
