4ef6f916e12bf4d3e12383d8059507353d2b565b
LulinPollux <kistssy+dev@gmail.com>
2020-04-10 11:18:50 +0900

    refactor: change print format
    % -> .format()

--- PyTorch Zero To All S1/2Linear Model.py
+++ PyTorch Zero To All S1/2Linear Model.py
@@ -22,17 +22,17 @@ w_list = []
 mse_list = []
 
 for w in np.arange(0.0, 4.1, 0.1):
-    print('w = %.1f' % w)
+    print('w = {:.1f}'.format(w))
     loss_sum = 0
 
     for x, y in zip(x_data, y_data):
         y_pred = forward(x)
         loss_val = loss(x, y)
         loss_sum += loss_val
-        print('\t%.1f %.1f %.1f %.1f' % (x, y, y_pred, loss_val))
+        print('\t{:.1f} {:.1f} {:.1f} {:.1f}'.format(x, y, y_pred, loss_val))
 
     mse = loss_sum / 3
-    print('MSE = %.2f' % mse)
+    print('MSE = {:.2f}'.format(mse))
     w_list.append(w)
     mse_list.append(mse)
 
